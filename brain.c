69699c56bcbbda8c02892151b17ac07749bdb8a3
Geo <geoolson@pdx.edu>
2018-07-06 15:04:25 -0700

    added a compile function that calls gcc and passes the translated program

--- brain.c
+++ brain.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
+#include <unistd.h>
 
-void foo(FILE * fp){
+//translate the brainfuck code to c
+void translate(FILE * fp){
   FILE * fpc;
   int c;
-  fpc = fopen("brains.c", "w");
+  fpc = fopen("out.c", "w");
   fputs("#include <stdio.h>\n", fpc);
   fputs("int main(){\n", fpc);
   fputs("char array[9000] = {0};\n", fpc);
   fputs("char *ptr=array;\n", fpc);
   while((c =fgetc(fp))!= EOF){
     switch(c){
-      case '>': fputs("++ptr;", fpc);
+      case '>': fputs("++ptr;\n", fpc);
                 break;
-      case '<': fputs("--ptr;", fpc);
+      case '<': fputs("--ptr;\n", fpc);
                 break;
-      case '+': fputs("++*ptr;", fpc);
+      case '+': fputs("++*ptr;\n", fpc);
                 break;
-      case '-': fputs("--*ptr;", fpc);
+      case '-': fputs("--*ptr;\n", fpc);
                 break;
-      case '.': fputs("putchar(*ptr);", fpc);
+      case '.': fputs("putchar(*ptr);\n", fpc);
                 break;
-      case ',': fputs("*ptr=getchar();", fpc);
+      case ',': fputs("*ptr=getchar();\n", fpc);
                 break;
-      case '[': fputs("while(*ptr){", fpc);
+      case '[': fputs("while(*ptr){\n", fpc);
                 break;
-      case ']': fputs("}", fpc);
+      case ']': fputs("}\n", fpc);
                 break;
       default :
                 break;
     }
   }
   fputs("}", fpc);
+  fclose(fpc);
+  fclose(fp);
+}
+
+//executes the c compiler with the translated c file
+void compile(char * filename){
+  char * args[3];
+  args[0] = "gcc";
+  args[1] = filename;
+  args[2] = NULL;
+  pid_t pid = fork();
+  if(pid == 0)
+    execvp(args[0], args);
 }
 
 int main(int argc, char * argv[]){
@@ -40,6 +55,7 @@ int main(int argc, char * argv[]){
   }
   FILE * fp;
   fp = fopen(argv[1], "r");
-  foo(fp);
+  translate(fp);
+  compile("out.c");
   return 0;
 }
